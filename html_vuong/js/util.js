36a96f3c53542dda53055a392e0838266771a886
game_mall <cardinnguyen85@yahoo.com>
2012-10-22 16:41:45 +0700

    inserted html

---
+++ html_vuong/js/util.js
@@ -0,0 +1,50 @@
+
+
+var Util = {
+        EMIT_REGISTRY_ACCOUNT   : 'registry_account'
+    ,   EMIT_LOGIN          : 'login_account'
+    ,   EMIT_PERSONAL_INFO  : 'personal_info'
+    ,   EMIT_LOBBY_PLAYERS  : 'lobby_player'
+    ,   EMIT_LEAVE_LOBBY    : 'leave_lobby'
+    ,   EMIT_JOIN_LOBBY     : 'join_lobby'
+    ,   EMIT_TABLE_LIST     : 'table_list'
+    ,   EMIT_CREATE_TABLE   : 'create_table'
+    ,   EMIT_JOIN_TABLE     : 'join_table'
+    ,   EMIT_LEAVE_TABLE    : 'leave_table'
+    ,   EMIT_HOST_CHANGE    : 'host_change'
+    ,   EMIT_EVENT_LOG      : 'event_log'
+    ,   EMIT_CHANGE_SLOT    : 'change_slot'
+    ,   EMIT_KICK_PLAYER    : 'kick_player'
+    ,   EMIT_CHANGE_BLIND   : 'change_blind'
+    ,   EMIT_GET_CHIP       : 'get_chip'
+    ,   EMIT_PLAYING_CHIP   : 'playing_chip'
+    ,   EMIT_RETURN_CHIP    : 'return_chip'
+    ,   EMIT_JOIN_MATCH     : 'join_match'
+    ,   EMIT_BE_VIEWER      : 'be_viewer'
+    ,   EMIT_UPDATE_N_PLAYER: 'update_number_player'
+    ,   EMIT_START_MATCH    : 'start_match'
+    ,   EMIT_CHAT_IN_TABLE  : 'chat_in_table'
+    ,   EMIT_QUIT_MATCH     : 'quit_match'
+    ,   EMIT_BEAT_MATCH     : 'beat_match'
+    ,   EMIT_CURRENT_TURN   : 'current_turn'
+    ,   EMIT_DEAL_CARD      : 'deal_card'
+    ,   EMIT_RESULT_MATCH   : 'result_match'
+    ,   EMIT_RESULT_CONTENT : 'result_content'
+    ,   EMIT_REFRESH_TABLE  : 'refresh_table'
+
+    ,   BEAT_FOLD           : 1
+    ,   BEAT_ALL_IN         : 2
+    ,   BEAT_CHECK          : 3
+    ,   BEAT_RAISE          : 4
+    ,   BEAT_BET            : 5
+    ,   BEAT_CALL           : 6
+
+    ,   TURN_NONE           : 0
+    ,   TURN_DEAL_IN_HAND   : 1
+    ,   TURN_THREE_CARD     : 2
+    ,   TURN_FOURTH_CARD    : 3
+    ,   TURN_FIFTH_CARD     : 4
+    ,   TURN_SHOW_DOWN      : 5
+};
+
+// exports.util = Util;
